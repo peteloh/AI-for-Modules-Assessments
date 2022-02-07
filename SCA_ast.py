f97a233c8655c8aa2653cb6bfa77f13fa1fe2b0d
Pete Loh <pete.lohh@gmail.com>
2022-02-07 17:08:53 +0000

    SCA AST and tokenisation testing

--- SCA_ast.py
+++ SCA_ast.py
@@ -1,30 +1,40 @@
 import ast
 
-# Demo code to parse
-code = """\
-sheep = ['Shawn', 'Blanck', 'Truffy']
-
-def get_herd():
-    herd = []
-    for a_sheep in sheep:
-        herd.append(a_sheep)
-    return Herd(herd=herd)
-
-class Herd:
-    def __init__(self, herd):
-        self.herd = herd
-
-    def shave(self, setting='SMOOTH'):
-        for sheep in self.herd:
-            print(f"Shaving sheep {sheep} on a {setting} setting")
+code1 = b"""
+x = 0
+while x<=10: # should run 10 times
+    x+=1
 """
 
+code2 = b"""
+for i in range(0,5):
+    continue
+"""
 
-class Example(ast.NodeVisitor):
+code3 = b"""
+z = False
+for i in range(0,5):
+    if z == True:
+        x = 0
+        while x<=10: # should run 10 times
+            x+=1
+"""
+
+class for_loop(ast.NodeVisitor):
     def visit_For(self, node):
         print(f"Visiting for loop at line {node.lineno}")
 
-tree = ast.parse(code)
-visitor = Example()
-visitor.visit(tree)
+class while_loop(ast.NodeVisitor):
+    def visit_While(self, node):
+        print(f"Visiting while loop at line {node.lineno}")
+
+check_code = code3
+
+visitor1 = for_loop()
+visitor2 = while_loop()
+
+tree = ast.parse(check_code)
+visitor1.visit(tree)
+visitor2.visit(tree)
+
 
